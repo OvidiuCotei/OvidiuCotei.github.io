3e7f6af54d7103301aac0219836e5bc821476059
Ovy Kode <ovidiu_metallica@yahoo.com>
2018-11-05 13:22:57 +0200

    acc

--- Lab I/orientation.js
+++ Lab I/orientation.js
@@ -1,5 +1,6 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.29.1";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.05.0";
 window.addEventListener("deviceorientation", on_device_orientation);
+window.addEventListener("deviceorientation", on_device_motion);
 
 function on_device_orientation(e)
 {
@@ -15,4 +16,11 @@ function on_device_orientation(e)
 	context.beginPath();
 	context.arc(canvas.width / 2 + e.gamma / 90 * (canvas.width / 2 - R), canvas.height / 2 + e.beta / 90 * (canvas.height / 2 - R), R, 0, 2 * Math.PI);
 	context.stroke();
+}
+
+function on_device_motion(e)
+{
+	document.getElementById("id_acc_z").innerHTML = e.accelerationIncludingGravity.z;
+	document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity.x;
+	document.getElementById("id_acc_y").innerHTML = e.accelerationIncludingGravity.y;
 }
