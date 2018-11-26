bd9586188828bb7bf073d5114efc05a9a994953f
Ovy Kode <ovidiu_metallica@yahoo.com>
2018-11-26 15:14:16 +0200

    update twi finger

--- Lab I/touch.js
+++ Lab I/touch.js
@@ -1,9 +1,10 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.26.5";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.26.7";
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
 canvas.addEventListener("touchmove", on_touch_move);
 var canvas_bounding_rect = canvas.getBoundingClientRect();
-var last_pos = {x: 0, y: 0};
+
+var last_pos_array = [];
 
 function on_touch_start(e)
 {
@@ -13,8 +14,10 @@ function on_touch_start(e)
 		context.beginPath();
 		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, e.changedTouches[i].pageY - canvas_bounding_rect.top, 10, 0, 2 * Math.PI);
 		context.stroke();
-		last_pos.x = e.changedTouches[i].pageX;
-		last_pos.y = e.changedTouches[i].pageY;
+		var last_pos = {x: 0, y: 0};
+
+		var last_pos = {x: e.changedTouches[i].pageX, y: e.changedTouches[i].pageY, id: e.changedTouches[i].identifier};
+		last_pos_array.push(last_pos);
 	}
 }
 
@@ -24,10 +27,15 @@ function on_touch_move(e)
 
 	for(var i = 0; i < e.changedTouches.length; i++)
 	{
+		var j = 0;
+		for(; j < last_pos_array.length; j++)
+			if(last_pos_array[j].id == e.changedTouches[i].identifier)
+				break;
+
 		var context = canvas.getContext("2d");
 		context.beginPath();
 		context.lineWidth = 20;
-		context.moveTo(last_pos.x - canvas_bounding_rect.left, last_pos.y - canvas_bounding_rect.top);
+		context.moveTo(last_pos_array[j].x - canvas_bounding_rect.left, last_pos_array[j].y - canvas_bounding_rect.top);
 		context.lineTo(e.changedTouches[i].pageX - canvas_bounding_rect.left, e.changedTouches[i].pageY - canvas_bounding_rect.top);
 		context.stroke();
 		context.beginPath();
@@ -35,7 +43,7 @@ function on_touch_move(e)
 
 		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, e.changedTouches[i].pageY - canvas_bounding_rect.top, 10, 0, 2 * Math.PI);
 		context.stroke();
-		last_pos.x = e.changedTouches[i].pageX;
-		last_pos.y = e.changedTouches[i].pageY;
+		last_pos_array[j].x = e.changedTouches[i].pageX;
+		last_pos_array[j].y = e.changedTouches[i].pageY;
 	}
 }
