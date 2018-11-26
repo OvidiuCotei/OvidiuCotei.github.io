e7d600a74f29e404b02af38a8ad81d49231edf87
Ovy Kode <ovidiu_metallica@yahoo.com>
2018-11-26 14:33:53 +0200

    change

--- Lab I/touch.js
+++ Lab I/touch.js
@@ -3,6 +3,7 @@ var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
 canvas.addEventListener("touchmove", on_touch_move);
 var canvas_bounding_rect = canvas.getBoundingClientRect();
+var last_pos = {x: 0, y: 0};
 
 function on_touch_start(e)
 {
@@ -10,15 +11,17 @@ function on_touch_start(e)
 	{
 		var context = canvas.getContext("2d");
 		context.beginPath();
-		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, changedTouches[i].pageY - canvas_bounding_rect.top, 10, 0, 2 * Math.PI);
+		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, e.changedTouches[i].pageY - canvas_bounding_rect.top, 10, 0, 2 * Math.PI);
 		context.stroke();
+		last_pos.x = e.changedTouches[i].pageX;
+		last_pos.y = e.changedTouches[i].pageY;
 	}
 }
 
 function on_touch_move(e)
 {
 	e.preventDefault();
-	
+
 	for(var i = 0; i < e.changedTouches.length; i++)
 	{
 		var context = canvas.getContext("2d");
