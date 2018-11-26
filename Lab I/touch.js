726e8af9480e63281a85bf801aa08a3b49cf14ab
Ovy Kode <ovidiu_metallica@yahoo.com>
2018-11-26 14:21:27 +0200

    modification

--- Lab I/touch.js
+++ Lab I/touch.js
@@ -1,6 +1,7 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.26.0";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.26.1";
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
+var canvas_bounding_rect = canvas.getBoundingClientRect();
 
 function on_touch_start(e)
 {
@@ -8,7 +9,7 @@ function on_touch_start(e)
 	{
 		var context = canvas.getContext("2d");
 		context.beginPath();
-		context.arc(e.changedTouches[i].pageX, changedTouches[i].pageY, 10, 0, 2 * Math.PI);
+		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, changedTouches[i].pageY - addEventListener.top, 10, 0, 2 * Math.PI);
 		context.stroke();
 	}
 }
