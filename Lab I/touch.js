0c83133ec8ce13d17ea9e6b7df0b6441264e0c03
Ovy Kode <ovidiu_metallica@yahoo.com>
2018-11-26 15:24:08 +0200

    splice

--- Lab I/touch.js
+++ Lab I/touch.js
@@ -1,7 +1,8 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.26.7";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.26.8";
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
 canvas.addEventListener("touchmove", on_touch_move);
+canvas.addEventListener("touchmove", on_touch_end);
 var canvas_bounding_rect = canvas.getBoundingClientRect();
 
 var last_pos_array = [];
@@ -46,4 +47,14 @@ function on_touch_move(e)
 		last_pos_array[j].x = e.changedTouches[i].pageX;
 		last_pos_array[j].y = e.changedTouches[i].pageY;
 	}
+}
+
+function on_touch_end(e)
+{
+	for(var i = 0; i < e.changedTouches.length; i++)
+		var j = 0;
+	for(; j < last_pos_array.length; j++)
+			if(last_pos_array[j].id == e.changedTouches[i].identifier)
+				break;
+			last_pos_array.splice(j, 1);
 }
