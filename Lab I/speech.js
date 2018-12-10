2c80d41af423d17699dc32dd09e321e2321237a0
Ovy Kode <ovidiu_metallica@yahoo.com>
2018-12-10 15:47:17 +0200

    all

---
+++ Lab I/speech.js
@@ -0,0 +1,21 @@
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.10.1";
+document.getElementById("id_speak").addEventListener("click", on_speak);
+
+var speak = new webkitSpeechRecognition();
+speak.onresult = on_result;
+speak.onspeechend = on_speech_end;
+speak.lang = "en-US";
+
+function on_speak(e)
+{
+	speak.start();
+}
+
+function on_speech_end()
+{
+	speak.stop();
+}
+
+function on_result(){
+	document.getElementById("id_text").innerHTML = e.results[0][0].transcript + " {" + e.results[0][0].confidence + "}";
+}
